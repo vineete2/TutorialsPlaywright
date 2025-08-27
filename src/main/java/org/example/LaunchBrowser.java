a8244138f05579202bfc87c93f7c0182a7c4ad87
vineete2 <vineete2@gmail.com>
2025-08-27 22:04:15 +0200

    Initial commit

---
+++ src/main/java/org/example/LaunchBrowser.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Browser;
+
+public class LaunchBrowser {
+
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false)
+            );
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev");
+            System.out.println(page.title());
+
+        }
+
+    }
+}
