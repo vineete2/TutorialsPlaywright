4f2c37cb1f0fa26687c3b33cbb6987221d96209f
vineete2 <vineete2@gmail.com>
2025-08-30 23:55:50 +0200

    login validations

---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.testng.annotations.Test;
+
+import java.nio.file.Paths;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class Login {
+
+
+    @Test
+    public void Verify_login() {
+        Playwright playwright = Playwright.create();
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(100)
+            );
+            Page page = browser.newPage();
+            page.navigate("https://app.vwo.com/#/login");
+            System.out.println(page.title());
+            page.locator("#login-username").fill("93npu2yyb@@esiix.com");
+            page.locator("#login-password").fill("Wingify@123");
+            page.click("#js-login-btn");
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(page.title())));
+            assertEquals("Dashboard",page.title());
+
+    }
+
+}
