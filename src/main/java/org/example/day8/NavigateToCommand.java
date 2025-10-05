3ee2bcb638107343d12163c93c41d40d0fb91053
vineete2 <vineete2@gmail.com>
2025-10-05 23:29:36 +0200

    NavigateToCommand

---
+++ src/main/java/org/example/day8/NavigateToCommand.java
@@ -0,0 +1,53 @@
+package org.example.day8;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+// reference: https://github.com/ebrahimhossaincse/Playwright-Tutorials-Java.git
+
+public class NavigateToCommand {
+    protected static String url = "https://demoqa.com/";
+
+    Playwright playwright;
+    BrowserType browserType;
+    protected Browser browser;
+    protected BrowserContext context;
+    protected Page page;
+
+    @BeforeSuite
+    public void startChromeBrowser() {
+        playwright = Playwright.create();
+        browserType = playwright.chromium();
+        browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext(new Browser.NewContextOptions());
+
+        page = browser.newPage();
+        System.out.println("**** Chrome Browser Version is : " + browser.version());
+    }
+
+    @BeforeClass
+    public void openUrl() throws InterruptedException {
+        page.navigate(url);
+        page.waitForLoadState();
+    }
+
+    @Test(priority = 0)
+    public void navigateToCommand() throws InterruptedException {
+        page.navigate("https://the-internet.herokuapp.com/challenging_dom");
+        Thread.sleep(3000);
+    }
+
+    @AfterSuite
+    public void closeChromeBrowser() {
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
