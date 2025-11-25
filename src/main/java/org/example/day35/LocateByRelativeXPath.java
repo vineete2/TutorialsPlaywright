aa6d4e9e49e1cae92039f4a56049317d90f58893
vineete2 <vineete2@gmail.com>
2025-11-25 18:51:12 +0100

    dynamicXPath LocateByRelativeXPath

---
+++ src/main/java/org/example/day35/LocateByRelativeXPath.java
@@ -0,0 +1,55 @@
+package org.example.day35;
+
+//src https://github.com/ebrahimhossaincse/Playwright-Tutorials-Java/blob/main/src/test/java/locators/dynamicxpath/LocateByRelativeXPath.java
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class LocateByRelativeXPath {
+    protected static String url = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+
+    Playwright playwright;
+    BrowserType browserType;
+    protected Browser browser;
+    protected BrowserContext context;
+    protected Page page;
+
+    @BeforeSuite
+    public void startChromeBrowser() {
+        playwright = Playwright.create();
+        browserType = playwright.chromium();
+        browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext(new Browser.NewContextOptions());
+
+        page = browser.newPage();
+        System.out.println("**** Chrome Browser Version is : " + browser.version());
+    }
+
+    @BeforeClass
+    public void openUrl() throws InterruptedException {
+        page.navigate(url);
+        page.waitForLoadState();
+    }
+
+    @Test
+    public void relativeXPath() throws InterruptedException {
+        ElementHandle relativeElement = page.querySelector("//input[@id='name']");
+        relativeElement.fill("John Doe");
+        Thread.sleep(3000);
+    }
+
+    @AfterSuite
+    public void closeChromeBrowser() {
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
