36860316ecc739ba0af8442cd90356a0a3b4f7f6
vineete2 <vineete2@gmail.com>
2025-12-11 17:25:55 +0100

    fixed pom issues

--- src/test/java/reportsGenerator/CucumberReportGenerator.java
+++ src/test/java/reportsGenerator/CucumberReportGenerator.java
@@ -9,21 +9,24 @@ import java.util.List;
 
 public class CucumberReportGenerator {
 
-    /*
-     *This is generating report
-     */
     public static void main(String[] args) {
-        
+
         File reportOutputDirectory = new File("target/cucumber-reports");
-        List<String> jsonFiles = Collections.singletonList("target/cucumber.json");
+
+        // Correct: use File, not List
+        File jsonFile = new File("target/cucumber.json");
+
         if (!jsonFile.exists()) {
-        System.err.println("Required file target/cucumber.json does not exist. Please generate cucumber results before running the report generator.");
-        System.exit(1);
+            System.err.println("Required file target/cucumber.json does not exist. Please generate cucumber results before running the report generator.");
+            System.exit(1);
         }
-        List<String> jsonFiles = Collections.singletonList(jsonFile.getPath());
+
+        // ReportBuilder expects List<String>
+        List<String> jsonFiles = Collections.singletonList(jsonFile.getAbsolutePath());
+
         Configuration config = new Configuration(reportOutputDirectory, "TutorialsPlaywright");
         ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, config);
+
         reportBuilder.generateReports();
     }
-
 }
